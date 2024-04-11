d9eabf74f487e368af4f466079756837f7e1708f
nev3rfind <donstankevicius@gmail.com>
2024-04-11 18:03:35 +0100

    Added upload logic; #2

--- app/Http/Controllers/CsvFileController.php
+++ app/Http/Controllers/CsvFileController.php
@@ -8,9 +8,19 @@ class CsvFileController extends Controller
 {
     public function upload(Request $request)
     {
-        $required->validate([
-            'csv-file' => 'required|file|mimes:csv,txt'
-        ]);
+        $messages = [
+            'csv_file.required' => 'You must provide a file.',
+            'csv_file.file' => 'The provided file was invalid.',
+            'csv_file.mimes' => 'Only CSV and TXT files are allowed.',
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'csv_file' => 'required|file|mimes:csv,txt',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
 
         $path = $request->file('csv-file')->store('csv-file');
 
